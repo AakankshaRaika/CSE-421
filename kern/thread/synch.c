34c9e55c2dce0cce325b6770455bd7cd5dd16737
aakankshaRaika <aakanksh@buffalo.edu>
2017-02-13 14:02:20 -0800

    Cloned myorgin

--- kern/thread/synch.c
+++ kern/thread/synch.c
@@ -154,20 +154,7 @@ lock_create(const char *name)
 		return NULL;
 	}
 
-<<<<<<< HEAD
-        lock->lk_wchan = wchan_create(lock->lk_name);
-        if (lock->lk_wchan == NULL) {
-                kfree(lock->lk_name);
-                kfree(lock);
-                return NULL;
-        }
-
-        lock_init(&lock->lk_lock);
-        lock->lk_count = initial_count;
-
-=======
 	HANGMAN_LOCKABLEINIT(&lock->lk_hangman, lock->lk_name);
->>>>>>> refs/remotes/staff/master
 
 	// add stuff here as needed
 
