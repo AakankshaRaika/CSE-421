057f9cc95eb0e7061f88dc6c63e9448d9356ca88
aakankshaRaika <aakanksh@buffalo.edu>
2017-02-22 13:59:48 -0800

    implementing in syscall.c fails

--- kern/arch/mips/syscall/syscall.c
+++ kern/arch/mips/syscall/syscall.c
@@ -110,6 +110,13 @@ syscall(struct trapframe *tf)
 		break;
 
 	    /* Add stuff here */
+            case SYS_open:
+		err = sys_open((char)tf->tf_a0,tf->tf_a1);
+		break;
+
+            case SYS_write:
+		err = sys_write(tf->tf_a0,(const void)tf->tf_a1,(size_t)tf->tf_a2);
+		break;
 
 	    default:
 		kprintf("Unknown syscall %d\n", callno);
