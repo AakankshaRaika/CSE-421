685c85396c97e5c4e7e31552573052fb6b1b7b37
aakankshaRaika <aakanksh@buffalo.edu>
2017-02-22 14:00:34 -0800

    implementing in syscall.c fails

--- kern/include/syscall.h
+++ kern/include/syscall.h
@@ -58,5 +58,6 @@ __DEAD void enter_new_process(int argc, userptr_t argv, userptr_t env,
 
 int sys_reboot(int code);
 int sys___time(userptr_t user_seconds, userptr_t user_nanoseconds);
-
+ssize_t sys_write(int fd, const void *buf, size_t buflen);
+int sys_open(const char *filename, int flags);
 #endif /* _SYSCALL_H_ */
