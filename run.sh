4fe814f1073f89dca929fb101d530a41060bcc80
Andrew Nenno <ajnenno2@buffalo.edu>
2017-03-01 16:05:24 +0000

    added run script

---
+++ run.sh
@@ -0,0 +1,31 @@
+cd $HOME/CSE-421/kern/compile/ASST2
+bmake depend
+
+if [ $? -eq 0 ] ; then
+		echo "INFO: bmake depend successful, executing bmake..."
+else
+		echo "INFO: error running bmake depend"
+		exit
+fi
+
+bmake
+
+if [ $? -eq 0 ] ; then
+		echo "INFO: bmake successful, executing bmake install..."
+else
+		echo "INFO: error running bmake"
+		exit
+fi
+
+bmake install
+
+if [ $? -eq 0 ] ; then
+		echo "INFO: bmake install successful, activating kernel..."
+else
+		echo "INFO: error running bmake install"
+		exit
+fi
+
+cd ~/CSE-421/root
+sys161 kernel
+
