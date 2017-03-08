fe0f3b8d2876e28352b8b42851c274b509f18e38
Andrew Nenno <ajnenno2@buffalo.edu>
2017-03-08 21:03:17 +0000

    stubbed out cases for file syscalls

--- kern/arch/mips/syscall/syscall.c
+++ kern/arch/mips/syscall/syscall.c
@@ -113,20 +113,36 @@ syscall(struct trapframe *tf)
 
 	    /* Add stuff here */
 
-            case SYS_open:
-		err = sys_open((const char *)tf->tf_a0,tf->tf_a1);
-		break;
+        case SYS_open:
+			err = sys_open((const char *)tf->tf_a0,tf->tf_a1);
+			break;
 
-            case SYS_write:
-		err = sys_write(tf->tf_a0,
-			        (const void *)tf->tf_a1,
-				(size_t)tf->tf_a2);
-		break;
+        case SYS_write:
+			err = sys_write(tf->tf_a0,(const void *)tf->tf_a1,(size_t)tf->tf_a2);
+			break;
 
+		case SYS_read:
+			err=sys_read(tf->tf_a0,(void *)tf->tf_a1,(size_t)tf->tf_a2);
+			break;
+		case SYS_lseek:
+			err=sys_lseek(tf->tf_a0, (off_t)tf->tf_a1, tf->tf_a2);
+			break;
+		case SYS_close:
+			err=sys_close(tf->tf_a0);
+			break;
+		case SYS_dup2:
+			err=sys_dup2(tf->tf_a0, tf->tf_a1);
+			break;
+		case SYS_chdir:
+			err=sys_chdir((const char *)tf->tf_a0);
+			break;
+		case SYS__getcwd:
+			err=sys__getcwd((char *)tf->tf_a0, (size_t)tf->tf_a1);
+			break;
 	    default:
-		kprintf("Unknown syscall %d\n", callno);
-		err = ENOSYS;
-		break;
+			kprintf("Unknown syscall %d\n", callno);
+			err = ENOSYS;
+			break;
 	}
 
 
